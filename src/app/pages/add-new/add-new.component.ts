9c4ee6450917d629caf66a4afe6cbfd79815cf0e
AMANBH0207 <amanbhardwaj052@gmail.com>
2024-04-06 23:31:25 +0530

    Updates

--- src/app/pages/add-new/add-new.component.ts
+++ src/app/pages/add-new/add-new.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { S1Service } from '../../Service/s1.service';
+import { NgbModule  } from '@ng-bootstrap/ng-bootstrap';
+import { NgToastModule, NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-add-new',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,NgbModule, NgToastModule],
   templateUrl: './add-new.component.html',
   styleUrl: './add-new.component.css'
 })
@@ -17,7 +19,12 @@ export class AddNewComponent implements OnInit {
 
   // Creating a reactive form and calling service to save data by initializing it inside constructer
   myForm: FormGroup | any;
-  constructor(private fb: FormBuilder, private s1Service: S1Service) { }
+  constructor(private fb: FormBuilder, private s1Service: S1Service, private toast: NgToastService) {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
   ngOnInit(): void {
     this.myForm = this.fb.group({
       name: ['', Validators.required],
@@ -35,6 +42,7 @@ export class AddNewComponent implements OnInit {
       this.s1Service.saveItem(this.myForm.value);
       // Reset the form after submitting
       this.myForm.reset();
+      this.openSuccessAdd()
     } else {
       console.log("Form is invalid");
     }
@@ -68,5 +76,16 @@ export class AddNewComponent implements OnInit {
       localStorage.setItem('Data', JSON.stringify(this.dataArray));
     }
     this.myForm.reset();
+    this.index=undefined;
+    this.s1Service.index=undefined;
+    this.openSuccessUpdate();
+  }
+
+  //functions for toast message
+  openSuccessUpdate(){
+    this.toast.success({detail:"Data has been updated",summary:"Now you can add new data here",duration:5000});
+  }
+  openSuccessAdd(){
+    this.toast.success({detail:"Data has been added",summary:"Now you can add more data here",duration:5000});
   }
 }
